1f65e2c683cafc2336dfd8f5d2783559557d4eaf
Ojrzyn <ojrzyn03@gmail.com>
2023-05-31 00:58:56 +0200

    Koncept

---
+++ scripts/deploy.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const lockedAmount = ethers.utils.parseEther("0.001");
+
+  const EtheralGold = await ethers.getContractFactory("EtheralGold");
+  const etheralGold = await EtheralGold.deploy(unlockTime, { value: lockedAmount });
+
+  await etheralGold.deployed();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
